37721e102649014971ca891788844623eab548e4
Felipe <felipeg.bueno@outlook.com>
2025-08-29 17:02:55 -0300

    add api key

---
+++ src/key-guard/key-guard.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly configService: ConfigService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    
+    const apiKey = request.headers['x-api-key'];
+    const validKey = this.configService.get<string>('API_KEY');
+
+    if (!apiKey || apiKey !== validKey) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
